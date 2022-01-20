1e5c30cc62daae625c2ec274e61060a35dd52802
Kev1nLopes <ketisinho14@gmail.com>
2022-01-20 14:00:59 -0300

    pequenos ajustes

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -89,7 +89,9 @@
 
 <script>
 import io from 'socket.io-client';
-const socket = io('http://149.28.53.210:3000');
+const socket = io('http://149.28.53.210:3000', {
+  withCredentials: true
+});
 let c = (el)=>document.querySelector(el);
 let cs = (el)=>document.querySelectorAll(el);
 
